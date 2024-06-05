a618c6358110889079da16a69d0e5d8e6f8ce1b3
Kireichev David <kireichev.david@gmail.com>
2024-06-05 10:22:21 +0500

    Сделал второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+using System;
+using System.Threading;
+
+class Program
+{
+    static int AckFun(int m, int n) 
+    { 
+        if (m == 0) 
+        {  
+            return n + 1; 
+        }  
+        else if (n == 0) 
+        {  
+            return AckFun(m - 1, 1); 
+        } 
+        else 
+        {  
+            return AckFun(m - 1, AckFun(m, n - 1)); 
+        }  
+    } 
+ 
+    static void Res() 
+    { 
+        int m = 2; 
+        int n = 3; 
+ 
+        int result = AckFun(m, n);  
+        Console.WriteLine($"Функция Аккермана равна {result}");  
+    } 
+
+    static void Run()
+    {
+        Res();
+    }
+
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(Run, 1024 * 1024 * 64); // 64 MB stack size
+        thread.Start();
+        thread.Join();
+    }
+}
