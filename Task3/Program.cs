0c91df747e0bb4c845420c5fa160d31f33c94072
Kireichev David <kireichev.david@gmail.com>
2024-06-05 10:22:38 +0500

    Сделал третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+int[] array = { 13, 29, 9, 45, 100 };
+
+Console.WriteLine("Элементы массива, начиная с конца:");
+PrintArrayReverse(array, array.Length - 1);
+
+void PrintArrayReverse(int[] arr, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(arr[index] + " ");
+        PrintArrayReverse(arr, index - 1);
+    }
+}
+
